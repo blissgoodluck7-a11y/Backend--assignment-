6edcb5354dae23131cc29a8ac34e8846a7a7e730
blissgoodluck7-a11y <blissgoodluck7@gmail.com>
2026-05-17 23:08:57 +0100

    Create Intro.js

---
+++ Intro.js
@@ -0,0 +1,7 @@
+const name = "Goodluck Bliss"; 
+const age = 19; 
+const favoriteLanguage = "JavaScript";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorite Programming Language: " + favoriteLanguage);
